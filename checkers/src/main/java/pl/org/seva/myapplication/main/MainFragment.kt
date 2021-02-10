dd93b9c9e658151a6c0f00c70efb0021b7543275
wiktor <wiktor.nizio@gmail.com>
2021-02-10 12:56:16 +0100

    Initial commit.

---
+++ checkers/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -0,0 +1,14 @@
+package pl.org.seva.myapplication.main
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import kotlinx.coroutines.*
+import pl.org.seva.myapplication.R
+
+class MainFragment : Fragment(R.layout.fr_main) {
+
+    @ObsoleteCoroutinesApi
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    }
+}
