cb428a44904347676da6e1b5d1c89d029618fded
wiktor <wiktor.nizio@gmail.com>
2021-02-16 16:56:07 +0100

    Drew board.

--- checkers/src/main/java/pl/org/seva/checkers/main/init/Bootstrap.kt
+++ checkers/src/main/java/pl/org/seva/checkers/main/init/Bootstrap.kt
@@ -2,13 +2,11 @@ package pl.org.seva.checkers.main.init
 
 import android.content.Context
 import androidx.appcompat.app.AppCompatDelegate
-import pl.org.seva.checkers.main.createNotificationChannels
 
 
 class Bootstrap {
 
     fun boot(ctx: Context) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-        createNotificationChannels(ctx)
     }
 }
--- checkers/src/main/java/pl/org/seva/checkers/view/Board.kt
+++ checkers/src/main/java/pl/org/seva/checkers/view/Board.kt
@@ -1,4 +1,31 @@
 package pl.org.seva.checkers.view
 
-class Board {
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class Board(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+    private val fill = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.BLACK
+        style = Paint.Style.FILL
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        with(canvas) {
+            val dx = right / 8f
+            val dy = bottom / 8f
+            repeat(8) { x ->
+                repeat(8) { y ->
+                    if (x % 2 != y % 2)
+                    drawRect(x * dx, y * dy, (x + 1) * dx, (y + 1) * dy, fill)
+                }
+            }
+        }
+    }
 }
