459c800ba7730d6e019ad7b6f2c84989959d6289
wiktor <wiktor.nizio@gmail.com>
2021-02-19 20:11:53 +0100

    Applied view binding.

--- checkers/src/main/java/pl/org/seva/checkers/main/MainFragment.kt
+++ checkers/src/main/java/pl/org/seva/checkers/main/MainFragment.kt
@@ -1,12 +1,32 @@
 package pl.org.seva.checkers.main
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import pl.org.seva.checkers.R
+import pl.org.seva.checkers.databinding.FrMainBinding
 
 class MainFragment : Fragment(R.layout.fr_main) {
 
+    private var _binding: FrMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FrMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
