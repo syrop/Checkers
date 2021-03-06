a52fb0366d1033422aae4a535ff933761216c908
wiktor <wiktor.nizio@gmail.com>
2021-03-06 07:45:21 +0100

    Added license.

--- checkers/src/main/java/pl/org/seva/checkers/main/extension/Context.kt
+++
@@ -1,7 +0,0 @@
-package pl.org.seva.checkers.main.extension
-
-import android.content.Context
-import android.content.SharedPreferences
-import android.preference.PreferenceManager
-
-val Context.prefs: SharedPreferences get() = PreferenceManager.getDefaultSharedPreferences(this)
