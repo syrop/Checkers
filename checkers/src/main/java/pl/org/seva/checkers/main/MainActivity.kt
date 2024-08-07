5776082ce12747c59b8f743fe25440d0821a85f3
wiktor <wiktor.nizio@gmail.com>
2024-08-07 03:41:04 +0200

    Removed data binding.

--- checkers/src/main/java/pl/org/seva/checkers/main/MainActivity.kt
+++ checkers/src/main/java/pl/org/seva/checkers/main/MainActivity.kt
@@ -35,8 +35,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater, null, false)
-        binding.lifecycleOwner = this
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
